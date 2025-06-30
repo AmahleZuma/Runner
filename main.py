d91dbc9afddeb84066c7d5cca85e68076f9a9a0a
Amahle <amahlez798@gmail.com>
2025-06-30 10:20:34 +0200

    Starting off the Running Game

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import exit
+
+# Switching pygame on
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+              
+    #draw all the elements and update everything
+    pygame.display.update()
+    clock.tick(60)
