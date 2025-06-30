30941f6986bc17031324fa1298d831460da15b30
Amahle <amahlez798@gmail.com>
2025-06-30 22:10:17 +0200

    Sleepy...like 'I need a coffee or I'll kill someone' sleepy...but hey I drew a square :)

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
 test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-              
+
+    #Block Image Transfer...one image on another        
+    screen.blit(test_surface,(0,0))
+
+
     #draw all the elements and update everything
     pygame.display.update()
     clock.tick(60)
