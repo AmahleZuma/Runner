aa9d4c268e590b32fae45a9c8e8715eadb57ef03
Amahle <amahlez798@gmail.com>
2025-07-02 12:52:37 +0200

    Using collide point to check if mouse has collided with the Runner, also adding a score board

--- Runner/main.py
+++ Runner/main.py
@@ -7,11 +7,13 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('Runner/font/Pixeltype.ttf', 50)
+score_font = pygame.font.Font('Runner/font/Pixeltype.ttf', 50)
 
 # 'Static' surface
 sky_surface = pygame.image.load('Runner/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('Runner/graphics/ground.png').convert()
 text_surface = test_font.render('RUNNER', False, 'Black')
+
 
 # 'Dynamic ' Surface
 snail_surface = pygame.image.load('Runner/graphics/snail/snail1.png').convert_alpha()
@@ -21,12 +23,23 @@ snail_rect = snail_surface.get_rect(midbottom = (600,300))
 player_surface = pygame.image.load('Runner/graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80,300))
 
+# Score
+score_surface = score_font.render(f'Score: {0}', False, 'Black')
+score_rect = score_surface.get_rect(center = (410,100))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type ==  pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('Collision!')
+                 
+
+            
+        
 
     #Block Image Transfer...one image on another        
 
@@ -35,18 +48,29 @@ while True:
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface, (350, 50))
 
+    # Drawing the score
+    screen.blit(score_surface, (score_rect))
+
     # Drawing the snail and using geometric tricks to give it a looping effect
     snail_rect.left -= 4
     if snail_rect.left <= -50: snail_rect.left = 900
     screen.blit(snail_surface, snail_rect)
 
     # Drawing the player
-    player_rect.left += 1
     screen.blit(player_surface, player_rect)
 
+
     # Checking if a collison has indeed occured 
-    if player_rect.colliderect(snail_rect):
-        print("Collision")
+    # Specifically if the rectangles are overlapping...this method is a massive issue for health bar related functions
+    # if player_rect.colliderect(snail_rect):
+    #     print("Collision")
+
+    # This determines whether or not the mouse pointer has collided with the player and what buttons were pressed exactly
+    # mouse_pos = pygame.mouse.get_pos()
+    # if  player_rect.collidepoint((mouse_pos)):
+    #     print(True)
+
+    
 
 
 
